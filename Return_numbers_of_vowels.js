06fb37527f1e6bff8af8d6caabc7ab244bdb05e0
ziaul-hassan <ziaulhasanf@gmail.com>
2025-03-22 15:08:49 +0600

    update the code

--- Return_numbers_of_vowels.js
+++ Return_numbers_of_vowels.js
@@ -1,8 +1,9 @@
 let string1="How are you";
-count=0;
-for(let i=1;i<string1.length;i++){
-    str=string1[i];
-    if(str=='a'|| str=='A'||str=='E'||str=='e'||str=='i'||str=='I'||str=='i'||str=='o'||str=='O'||str=='u'||str=='U')
+let count=0;
+let vowel=new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+for(let i=0;i<string1.length;i++){
+    if(vowel.has(string1[i])){
         count++;
+    }
 }
 console.log(count);
