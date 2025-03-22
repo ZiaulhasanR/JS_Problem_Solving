1ae5714bb502296d98d711940da0c4474204d36c
ziaul-hassan <ziaulhasanf@gmail.com>
2025-03-22 21:48:52 +0600

    nothing

--- calculate_sum_of_array.js
+++ calculate_sum_of_array.js
@@ -1,5 +1,5 @@
 let $numbers = [3, 4, 50, 5, 3, 10, 4, 6, 7, 8, 0, 8, 10, 1, 9, 32, 67, 9, 10];
 const totalSum = $numbers.reduce((accumulator, number) => {
-  return accumulator * number;
-}, 1);
+  return accumulator + number;
+}, 0);
 console.log(totalSum);
